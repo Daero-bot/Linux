c60c452d2f2f64d9afeb37339b38980fafae5db0
Juan Daniel <117939198+JuanDaniel-SMR@users.noreply.github.com>
2026-05-06 13:30:41 +0200

    Refactor calculator script to use numeric options

--- scripts/calculadora.sh
+++ scripts/calculadora.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
-echo "Bienvenido a mi calculadora."
-read -p "Deseas SUMAR, RESTAR, MULTIPLICAR o DIVIDIR?."$'\n' op
-
-if [ "$op" = "SUMAR" ]; then
-    read -p "Has elegido sumar, escriba el primer número."$'\n' num1
+# Programa hecho por Juan Daniel Porras Navarrete 
+clear
+echo " "
+echo "                           -------------------   "
+echo "                           | Calculadora 2.1 |   "
+echo "                           -------------------   "
+echo " "
+echo "Eliga que operación desea hacer."
+echo " "
+echo "[1] SUMAR "
+echo "[2] RESTAR "
+echo "[3] MULTIPLICAR "
+echo "[4] DIVIDIR "
+echo " "
+read -p "Operación que desea hacer >> " operation
+echo " "
+if [ "$operation" = "1" ]; then #Operacion SUMA
+    read -p "Has elegido sumar, escriba el primer número. >> " num1
     if [[ "$num1" =~ ^-?[0-9]+$ ]]; then
-        read -p "Tu primer número es $num1, escriba el siguiente numero."$'\n' num2
+        read -p "Tu primer número es $num1, escriba el siguiente numero. >> " num2
         if [[ "$num2" =~ ^-?[0-9]+$ ]]; then
             resultado=$((num1 + num2))
+            echo " "
             echo "El resultado de tu suma es $resultado."
         else
             echo "Tienes que escribir un número válido."
@@ -16,12 +30,13 @@ if [ "$op" = "SUMAR" ]; then
         echo "Tienes que escribir un número válido."
     fi
 
-elif [ "$op" = "RESTAR" ]; then
-    read -p "Has elegido restar, escriba el primer número."$'\n' num1
+elif [ "$operation" = "2" ]; then #Operacion RESTA
+    read -p "Has elegido restar, escriba el primer número. >> " num1
     if [[ "$num1" =~ ^-?[0-9]+$ ]]; then
-        read -p "Tu primer número es $num1, escriba el siguiente numero."$'\n' num2
+        read -p "Tu primer número es $num1, escriba el siguiente numero. >> " num2
         if [[ "$num2" =~ ^-?[0-9]+$ ]]; then
             resultado=$((num1 - num2))
+	    echo " "
             echo "El resultado de tu resta es $resultado."
         else
             echo "Tienes que escribir un número válido."
@@ -30,12 +45,13 @@ elif [ "$op" = "RESTAR" ]; then
         echo "Tienes que escribir un número válido."
     fi
 
-elif [ "$op" = "MULTIPLICAR" ]; then
-    read -p "Has elegido multiplicar, escriba el primer número."$'\n' num1
+elif [ "$operation" = "3" ]; then #Operacion MULTIPLICAR
+    read -p "Has elegido multiplicar, escriba el primer número. >> " num1
     if [[ "$num1" =~ ^-?[0-9]+$ ]]; then
-        read -p "Tu primer número es $num1, escriba el siguiente numero."$'\n' num2
+        read -p "Tu primer número es $num1, escriba el siguiente numero. >> " num2
         if [[ "$num2" =~ ^-?[0-9]+$ ]]; then
             resultado=$((num1 * num2))
+	    echo " "
             echo "El resultado de tu multiplicación es $resultado."
         else
             echo "Tienes que escribir un número válido."
@@ -44,13 +60,14 @@ elif [ "$op" = "MULTIPLICAR" ]; then
         echo "Tienes que escribir un número válido."
     fi
 
-elif [ "$op" = "DIVIDIR" ]; then
-    read -p "Has elegido dividir, escriba el primer número."$'\n' num1
+elif [ "$operation" = "4" ]; then #Operacion DIVIDIR
+    read -p "Has elegido dividir, escriba el primer número. >> " num1
     if [[ "$num1" =~ ^-?[0-9]+$ ]]; then
-        read -p "Tu primer número es $num1, escriba el siguiente numero."$'\n' num2
+        read -p "Tu primer número es $num1, escriba el siguiente numero. >> " num2
         if [[ "$num2" =~ ^-?[0-9]+$ ]]; then
             if [ "$num2" -ne 0 ]; then
                 resultado=$((num1 / num2))
+		echo " "
                 echo "El resultado de tu división es $resultado."
             else
                 echo "No se puede dividir entre 0."
@@ -63,6 +80,6 @@ elif [ "$op" = "DIVIDIR" ]; then
     fi
 
 else
-    echo "Opción no valida. Solo puedes elegir: SUMAR, RESTAR, MULTIPLICAR o DIVIDIR."
-    echo "Recuerda tener cuidado con las mayúsculas."
+    echo "Opción no valida. Solo puedes elegir: 1,2,3 o 4."
 fi
+echo " "
